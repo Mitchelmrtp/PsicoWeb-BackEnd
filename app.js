f1e3f094159e2ed514c04b736c3bae172ad9744c
Mitchel Torres <82741018+Mitchelmrtp@users.noreply.github.com>
2025-05-12 15:52:44 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const authRoutes = require('./routes/auth.routes');
-
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-
-module.exports = app;
