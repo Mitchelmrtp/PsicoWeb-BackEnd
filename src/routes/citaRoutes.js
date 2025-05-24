4ab7ad05d582561a482178766d796fa6d55af827
Daniel Arenas <dan.arecalvo@gmail.com>
2025-05-24 13:54:53 -0500

    cambios

--- src/routes/citaRoutes.js
+++ src/routes/citaRoutes.js
@@ -5,5 +5,6 @@ import authMiddleware from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.post("/crear", authMiddleware, citaController.crearCita);
+router.put("/reprogramar", authMiddleware, citaController.reprogramarCita);
 
 export default router;
