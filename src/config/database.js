6fae2809de3a85fe0abe162b5a3de453b4b54728
Mitchel Torres <mitchel.mrtp@gmail.com>
2025-07-06 03:29:48 -0500

    ref

--- src/config/database.js
+++ src/config/database.js
@@ -10,13 +10,13 @@ const sequelize = new Sequelize(
     dialect: 'postgres',
     logging: false,
     pool: {
-      max: 5,           // Maximum number of connections in pool
-      min: 0,           // Minimum number of connections in pool
-      acquire: 30000,   // Maximum time (ms) that pool will try to get connection before throwing error
-      idle: 10000       // Maximum time (ms) that a connection can be idle before being released
+      max: 5,           
+      min: 0,           
+      acquire: 30000,   
+      idle: 10000       
     },
     retry: {
-      max: 3           // Maximum retry attempts when database query fails
+      max: 3           
     }
   }
 );
